5bbee94848cc8ca10fa6fc59b7900edd4dbed3e6
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-12-22 14:54:56 -0500

    [update] fill with gnu

--- controller/datosListadoPlanillaAsistencia2.php
+++ controller/datosListadoPlanillaAsistencia2.php
@@ -40,6 +40,51 @@
           "FECHA_REEMPLAZO" => "",
         ];
 
+        $found = [];
+        foreach ($lstPersonalEstado as $personalEstado) {
+          if ($idPersonal == $personalEstado['IDPERSONAL']) {
+            $found = $personalEstado;
+          }
+        }
+
+        /* Begin - Cargo Generico Unificado B */
+        $select = "<select id='planilla-select-col8-$idPersonal' class='planilla-select-col8'>";
+        foreach ($crgman as $item) {
+          $idCgu = $item['IDCARGO_GENERICO_UNIFICADO'];
+          $cgu = $item['CARGO_GENERICO_UNIFICADO'];
+          $select = $select . ($personalEstado['IDCARGO_GENERICO_UNIFICADO_B'] == $idCgu
+            ? "<option value='$idCgu' selected>$cgu</option>"
+            : "<option value='$idCgu'>$cgu</option>");
+        }
+        $select = $select . "</select>";
+        $row['CARGO_GENERICO_UNIFICADO_B'] = $select;
+        /* End - Cargo Generico Unificado B */
+
+        /* Begin - Clasificacion */
+        $row['CLASIFICACION_B_TEXT'] = $found['CLASIFICACION_B'];
+        $row['CLASIFICACION_B'] = "<span id='planilla-text-col9-$idPersonal'>" . $found['CLASIFICACION_B'] . "</span>";
+        /* End - Clasificacion */
+
+        /* Begin - Referencia 1 */
+        $row['REFERENCIA1_B_TEXT'] = $found['REFERENCIA1_B'];
+        $row['REFERENCIA1_B'] = "<span id='planilla-text-col10-$idPersonal'>" . $found['REFERENCIA1_B'] . "</span>";
+        /* End - Referencia 1 */
+
+        /* Begin - Referencia 2 */
+        $select = "<select id='planilla-select-col11-$idPersonal' class='planilla-select-col11'>";
+        foreach ($refs2 as $item) {
+          $idRef2 = $item['IDREFERENCIA2'];
+          $ref2 = $item['REFERENCIA2'];
+          if ($item['IDREFERENCIA1'] == $found['IDREFERENCIA1_B']) {
+            $select = $select . ($found['IDREFERENCIA2_B'] == $idRef2
+              ? "<option value='$idRef2' selected>$ref2</option>"
+              : "<option value='$idRef2'>$ref2</option>");
+          }
+        }
+        $select = $select . "</select>";
+        $row['REFERENCIA2_B'] = $select;
+        /* End - Referencia 2 */
+
         /* Begin - Week */
         $col = 13;
         foreach ($lstDiasSemana as $dia) {
