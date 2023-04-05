e0a4594ec8f1ccf85d4ee1dcb3b95d6dd86b9dd4
al3xisvg <vasquezgarciaalexis@gmail.com>
2023-04-04 19:46:19 -0500

    [update] validar rut

--- view/js/helpers/validations.js
+++ view/js/helpers/validations.js
@@ -1,4 +1,4 @@
-function validarRUT(text) {
+/*function validarRUT(text) {
   // Remove any non-digit characters from the RUT
   var rut = text.replace(/\D/g, "");
 
@@ -25,6 +25,23 @@ function validarRUT(text) {
   if (calculatedVerificationDigit === 10)
     return verificationDigit === "K" || verificationDigit === "k";
   return verificationDigit === calculatedVerificationDigit;
+}*/
+
+function validarRUT(rutCompleto) {
+  if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test(rutCompleto)) return false;
+  var tmp = rutCompleto.split("-");
+  var digv = tmp[1];
+  var rut = tmp[0];
+  if (digv == "K") digv = "k";
+
+  return validarRUTAux(rut) == digv;
+}
+
+function validarRUTAux(T) {
+  var M = 0,
+    S = 1;
+  for (; T; T = Math.floor(T / 10)) S = (S + (T % 10) * (9 - (M++ % 6))) % 11;
+  return S ? S - 1 : "k";
 }
 
 function validarEmail(email) {
