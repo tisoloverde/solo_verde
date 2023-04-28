f0c0c2342497bb23ef8d17b043a063514e81a9ed
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-04-27 23:36:55 -0400

    [update] add con. PRD.

--- model/conexion_api.php
+++ model/conexion_api.php
@@ -1,10 +1,17 @@
 <?php
 	// Conectando a la base de datos
 	function conectar_api(){
+		// QA
+		// $user = "AppConnect";
+		// $pass = "TCw4etVPjEcqTRkGOqG8";
+		// $db = "GENERICA";
+		// $host = "ls-c325378e153b6215b55eceeb3bd07a3f8ae76ca2.chtrgv4wtigr.us-east-1.rds.amazonaws.com";
+
+		// Producción
 		$user = "AppConnect";
 		$pass = "TCw4etVPjEcqTRkGOqG8";
 		$db = "GENERICA";
-		$host = "ls-c325378e153b6215b55eceeb3bd07a3f8ae76ca2.chtrgv4wtigr.us-east-1.rds.amazonaws.com";
+		$host = "10.0.0.8";
 
 		mysqli_report(MYSQLI_REPORT_STRICT);
 		try
