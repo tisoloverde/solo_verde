d0611cb09ed65f0449578ac8c6c099ab2d7a138d
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-04-27 23:39:03 -0400

    [update[ set con. to QA

--- model/conexion.php
+++ model/conexion.php
@@ -4,17 +4,17 @@
 
 	// Conectando a la base de datos
 	function conectar(){
-		// // QA
-		// $user = "AppConnect";
-		// $pass = 'TCw4etVPjEcqTRkGOqG8';
-		// $db = "SOLO_VERDE";
-		// $host = "ls-c325378e153b6215b55eceeb3bd07a3f8ae76ca2.chtrgv4wtigr.us-east-1.rds.amazonaws.com";
+		// QA
+		$user = "AppConnect";
+		$pass = 'TCw4etVPjEcqTRkGOqG8';
+		$db = "SOLO_VERDE";
+		$host = "ls-c325378e153b6215b55eceeb3bd07a3f8ae76ca2.chtrgv4wtigr.us-east-1.rds.amazonaws.com";
 
 		// Producción
-		$user = "AppConnect";
-		$pass = "TCw4etVPjEcqTRkGOqG8";
-		$db = "GENERICA";
-		$host = "10.0.0.8";
+		// $user = "AppConnect";
+		// $pass = "TCw4etVPjEcqTRkGOqG8";
+		// $db = "GENERICA";
+		// $host = "10.0.0.8";
 
 		mysqli_report(MYSQLI_REPORT_STRICT);
 		try
