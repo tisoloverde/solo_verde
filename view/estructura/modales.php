14409db39094623801d2bf46c9efba1794e7f166
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-05-02 23:00:35 -0400

    [update] idload 45

--- view/estructura/modales.php
+++ view/estructura/modales.php
@@ -9,7 +9,7 @@
 	  return str;
 	}
   var js = document.createElement('script');
-  js.src = 'view/js/funciones.js?idload=44';
+  js.src = 'view/js/funciones.js?idload=45';
   document.getElementsByTagName('head')[0].appendChild(js);
 </script>
 
