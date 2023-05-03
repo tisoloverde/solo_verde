c2736d98d1e1f8005d204f975542ab6e6f6f674d
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-05-03 00:20:03 -0400

    [update] fix asistencia

--- controller/ingresaMultiseleccionPermisos.php
+++ controller/ingresaMultiseleccionPermisos.php
@@ -1,5 +1,5 @@
 <?php
-    // //ini_set('display_errors', 'On');
+    // ini_set('display_errors', 'On');
     require('../model/consultas.php');
     session_start();
     if(count($_POST) > 0){
@@ -11,19 +11,27 @@
         $row = '';
         borrarPermisosJefaturaTodos($idPerfil, $idAreaWeb);
         borrarPermisosMultiselect($idPerfil, $idAreaWeb);
-        if (count($data[3]) == 0){
+        $a = 0;
+        $b = 0;
+        if (!array_key_exists(3, $data)){
           for($i = 0; $i < count($data[2]); $i++){
               $idAreaFuncional = $data[2][$i];
               $row = ingresaAreaFuncionalMultiSel($idAreaWeb, $idPerfil, $idAreaFuncional);
           }
         }
-        if (count($data[2]) == 0){
+        else{
+          $a = 1;
+        }
+        if (!array_key_exists(2, $data)){
           for($i = 0; $i < count($data[3]); $i++){
               $idEstructura = $data[3][$i];
               $row = ingresaProyectosMultiSel($idAreaWeb, $idPerfil, $idEstructura);
           }
         }
         else{
+          $b = 1;
+        }
+        if($a == 1 && $b == 1){
           for($j = 0; $j < count($data[3]); $j++){
             for($i = 0; $i < count($data[2]); $i++){
                 $idAreaFuncional = $data[2][$i];
