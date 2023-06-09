edae634049fcdb2fcc684993a3fc3a382bbaebca
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-06-08 22:51:42 -0400

    [update] fix

--- controller/solicitudInformeRexmas.php
+++ controller/solicitudInformeRexmas.php
@@ -9,14 +9,14 @@
 
   $rutUser = $_SESSION['rutUser'];
   $row = chequeaUsuarioEmail($rutUser);
+	$tipo = $_POST['tipo'];
 
-	var_dump($_POST);
+	// var_dump($_POST);
 
 	if($tipo == "faltas"){
 		$ceco = $_POST['ceco'];
 	  $fechaInicio = $_POST['fechaInicio'];
 	  $fechaFin = $_POST['fechaFin'];
-		$tipo = $_POST['tipo'];
 		exec('php -f ' . $ruta . 'controller/generaInformeRexmas.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $fechaInicio . ' ' . $fechaFin . ' > /dev/null 2>&1 &');
 	}
 	else if($tipo == "heAtrasos"){
@@ -26,7 +26,6 @@
 		$ceco = $_POST['ceco'];
 	  $fechaInicio = $_POST['fechaInicio'];
 	  $fechaFin = $_POST['fechaFin'];
-		$tipo = $_POST['tipo'];
 
 		exec('php -f ' . $ruta . 'controller/generaInformeRexmasHE.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $fechaInicio . ' ' . $fechaFin . ' ' . $he50 . ' ' . $he100 . ' ' . $atraso . ' > /dev/null 2>&1 &');
 	}
@@ -34,16 +33,14 @@
 		$ceco = $_POST['ceco'];
 	  $fechaInicio = $_POST['fechaInicio'];
 	  $fechaFin = $_POST['fechaFin'];
-		$tipo = $_POST['tipo'];
 		exec('php -f ' . $ruta . 'controller/generaInformeRexmasGeneral.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $fechaInicio . ' ' . $fechaFin . ' > /dev/null 2>&1 &');
 	}
 	else if($tipo == "generalMensual"){
 		$ceco = $_POST['ceco'];
 	  $anoMes = $_POST['anoMes'];
-		$tipo = $_POST['tipo'];
 		exec('php -f ' . $ruta . 'controller/generaInformeRexmasGeneralMensual.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $anoMes . ' > /dev/null 2>&1 &');
 
-		echo 'php -f ' . $ruta . 'controller/generaInformeRexmasGeneralMensual.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $anoMes . ' > /dev/null 2>&1 &';
+		// echo 'php -f ' . $ruta . 'controller/generaInformeRexmasGeneralMensual.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $anoMes . ' > /dev/null 2>&1 &';
 	}
 	echo "Ok";
 ?>
