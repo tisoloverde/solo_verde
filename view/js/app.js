9bb1bc553200121fabbd8e68e4179c8db46f9b26
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-08-04 01:38:40 -0400

    [update] fix dash 2

--- view/js/app.js
+++ view/js/app.js
@@ -3298,7 +3298,7 @@ app.controller("indicadorAusentismoController", function(){
 
         var d = new Date();
         var ano = d.getFullYear();
-        var mes = d.getMonth() + 1;
+        var mes = d.getMonth();
 
         $("#ausentismoBI").attr("src",'https://lookerstudio.google.com/embed/u/0/reporting/1d0af53c-4379-4b05-8955-2d45d4a7860b/page/SAyWD?params={"df5":"include%EE%80%801%EE%80%80IN%EE%80%80' + ano.toString() + '","df6":"include%EE%80%800%EE%80%80IN%EE%80%80' + mesesArray[mes.toString()] + '","df7":"include%EE%80%800%EE%80%80IN%EE%80%80Mensual"}');
 
