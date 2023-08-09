825624d9221c03053d4c3962328c9a795d5ccfc7
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-08-09 15:12:27 -0400

    [update] test desarrollo GCP

--- model/conexion.php
+++ model/conexion.php
@@ -4,17 +4,17 @@
 
 	// Conectando a la base de datos
 	function conectar(){
-		// QA
-		// $user = "AppConnect";
-		// $pass = 'TCw4etVPjEcqTRkGOqG8';
-		// $db = "SOLO_VERDE";
-		// $host = "ls-c325378e153b6215b55eceeb3bd07a3f8ae76ca2.chtrgv4wtigr.us-east-1.rds.amazonaws.com";
-
-		// Producción
+		//GCP - Desarrollo
 		$user = "AppConnect";
-		$pass = "TCw4etVPjEcqTRkGOqG8";
+		$pass = 'TCw4etVPjEcqTRkGOqG8';
 		$db = "SOLO_VERDE";
-		$host = "10.0.0.8";
+		$host = "10.138.112.3";
+
+		// // Producción
+		// $user = "AppConnect";
+		// $pass = "TCw4etVPjEcqTRkGOqG8";
+		// $db = "SOLO_VERDE";
+		// $host = "10.0.0.8";
 
 		mysqli_report(MYSQLI_REPORT_STRICT);
 		try
--- model/conexion_api.php
+++ model/conexion_api.php
@@ -1,17 +1,17 @@
 <?php
 	// Conectando a la base de datos
 	function conectar_api(){
-		// QA
+		//GCP - Desarrollo
+		$user = "AppConnect";
+		$pass = 'TCw4etVPjEcqTRkGOqG8';
+		$db = "GENERICA";
+		$host = "10.138.112.3";
+
+		// Producción
 		// $user = "AppConnect";
 		// $pass = "TCw4etVPjEcqTRkGOqG8";
 		// $db = "GENERICA";
-		// $host = "ls-c325378e153b6215b55eceeb3bd07a3f8ae76ca2.chtrgv4wtigr.us-east-1.rds.amazonaws.com";
-
-		// Producción
-		$user = "AppConnect";
-		$pass = "TCw4etVPjEcqTRkGOqG8";
-		$db = "GENERICA";
-		$host = "10.0.0.8";
+		// $host = "10.0.0.8";
 
 		mysqli_report(MYSQLI_REPORT_STRICT);
 		try
