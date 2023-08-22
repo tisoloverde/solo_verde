4d74cb1eca0d6bdf0530904061577fb087e1b1ac
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-08-22 02:11:14 -0400

    [update] fix

--- controller/generaInformeRexmasGeneralMensual.php
+++ controller/generaInformeRexmasGeneralMensual.php
@@ -1,5 +1,5 @@
 <?php
-  // ini_set('display_errors', 'On');
+  ini_set('display_errors', 'On');
   // ok
   date_default_timezone_set("America/Santiago");
   $ruta = str_replace("controller", "", getcwd()) . '/';
