33bf0fd6390132bc72172884297ac2fa17ffa7a2
Mauricio Yañez <mauroya33@gmail.com>
2023-08-22 11:22:48 -0400

    fix datos dashboard

--- controller/dashboard/dashboard_1.php
+++ controller/dashboard/dashboard_1.php
@@ -2,6 +2,9 @@
   require('../../model/consultas_dashboard.php');
   session_start();
 
+    /*DAtos para aumentar el tiempo de repuesta ya que se cae porque la consulta dura mas de 1 minuto */
+    set_time_limit(180); // 3 minutos (en segundos)
+  
 	if($_GET['id'] == "QHhUYKGTAkX5aKWaUFtxFgNhQMXPS4"){
     if($_GET['informe'] == '1'){
       $row = datosDashboard1();
