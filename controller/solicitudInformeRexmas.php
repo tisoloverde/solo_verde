df8ba3ffa5583b038a47630296c6c5329d4a3e45
mauroya33 <mauroya33@gmail.com>
2023-08-28 12:29:48 -0400

    [fix]

--- controller/solicitudInformeRexmas.php
+++ controller/solicitudInformeRexmas.php
@@ -44,8 +44,8 @@
 	}	else if($tipo == "validacionMensual"){
 		$ceco = $_POST['ceco'];
 	  $anoMes = $_POST['anoMes'];
-		exec('php -f ' . $ruta . 'controller/generaInformeValidacionGeneralMensual.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $anoMes . ' > /dev/null 2>&1 &');
-		echo 'php -f ' . $ruta . 'controller/generaInformeValidacionGeneralMensual.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $anoMes . ' > /dev/null 2>&1 &';
+		exec('php -f ' . $ruta . 'controller/generaInformeValidacionMensual.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $anoMes . ' > /dev/null 2>&1 &');
+		echo 'php -f ' . $ruta . 'controller/generaInformeValidacionMensual.php ' . $rutUser . ' ' . $row['EMAIL'] . ' ' . $ceco . ' ' . $anoMes . ' > /dev/null 2>&1 &';
 
 	}
 	echo "Ok";
