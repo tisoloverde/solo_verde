a64a6c78fa2aaf0474623fe4217cdaa432a1cd49
mauricioyanez <myanez@cimaurbano.cl>
2023-12-06 17:03:52 -0300

    [update] FIx timeout a 2400

--- controller/dashboard/dashboard_1.php
+++ controller/dashboard/dashboard_1.php
@@ -3,7 +3,7 @@
   session_start();
 
     /*DAtos para aumentar el tiempo de repuesta ya que se cae porque la consulta dura mas de 1 minuto */
-    set_time_limit(1200); // 10 minutos (en segundos)
+    set_time_limit(2400); // 10 minutos (en segundos)
   
 	if($_GET['id'] == "QHhUYKGTAkX5aKWaUFtxFgNhQMXPS4"){
     if($_GET['informe'] == '1'){
